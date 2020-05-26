af708409791eeb240802c9b8f116a8c14bd5370f
Rami Mounla <voodoor@hotmail.com>
2020-05-26 21:44:45 +1200

    Updated sensor code - ready for testing

--- MotionAndLight/MotionAndLight.ino
+++ MotionAndLight/MotionAndLight.ino
@@ -1,9 +1,20 @@
+int sensorPin = 0; // the sensor PIN
+int lightPin = 1; // the toggle PIN
+int sensorValue = 0; // the sensor Value
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(sensorPin, INPUT);
+  digitalWrite(sensorPin, HIGH); //Pull down
 
+  pinMode(lightPin, OUTPUT);
 }
 
-void loop() {
+void loop() {  
   // put your main code here, to run repeatedly:
-
+  delay(1000); 
+  
+  sensorValue = digitalRead(sensorPin);
+  digitalWrite(lightPin, !sensorValue); 
+  delay(sensorValue*4000); 
 }
